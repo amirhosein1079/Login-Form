f9eabcdab0d3aa0460b83aa05194243b7bfa589a
Amirhosein <amirhosein.azizi1079@gmail.com>
2021-08-04 13:17:46 +0430

    Done

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let form = document.querySelector(".my-form ");
-const logBtn = document.querySelector(".confirm");
 const loginForm = document.querySelector(".login-form");
-const formGroup = document.querySelector(".form-group");
 const email = document.getElementById("email");
 const pass = document.getElementById("password");
+const forgetPassLink = document.querySelector(".forget-pass");
+const modalBg = document.querySelector(".modal-message-container");
+const recoverPassBtn = document.querySelector(".recover-pass-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modalForm = document.querySelector(".recover-pass");
 
 document.addEventListener("DOMContentLoaded", () => {
   loginForm.classList.add("appear-form");
@@ -15,34 +18,45 @@ form.addEventListener("submit", (e) => {
   checkInput();
 });
 
-function checkInput(){
+function checkInput() {
   const emailValue = email.value.trim();
   const passwordValue = pass.value.trim();
 
-  if(emailValue == ''){
-    setError(email , 'Email can not be blank !');
+  if (emailValue == "") {
+    setError(email, "Email can not be blank !");
   } else {
     setSuccess(email);
   }
 
-  if(passwordValue == '') {
-    setError(pass , 'Please enter your password !');
+  if (passwordValue == "") {
+    setError(pass, "Please enter your password !");
   } else {
-    setSuccess(pass)
+    setSuccess(pass);
   }
 }
 
-function setError(input , message){
+function setError(input, message) {
   const formGroup = input.parentElement;
-  const smallErrorMessage = formGroup.querySelector('.error-message');
+  const smallErrorMessage = formGroup.querySelector(".error-message");
   smallErrorMessage.innerText = message;
-  formGroup.classList.add('error');
-  formGroup.classList.remove('success');
+  formGroup.classList.add("error");
+  formGroup.classList.remove("success");
 }
 
-function setSuccess(input){
+function setSuccess(input) {
   const formGroup = input.parentElement;
-  formGroup.classList.add('success');
-  formGroup.classList.remove('error');
+  formGroup.classList.add("success");
+  formGroup.classList.remove("error");
 }
 
+forgetPassLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalBg.classList.add("bg-active");
+  loginForm.style.display = "none";
+});
+
+closeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalBg.classList.remove("bg-active");
+  loginForm.style.display = "block";
+});
